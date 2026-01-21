60c0ebc5aad939d8fb70dd0a01a5235aeed12639
Alex <agatter@htl-steyr.ac.at>
2026-01-21 11:54:01 +0100

    new

--- JavaFXJDBC/src/main/java/org/example/javafxjdbc/HelloController.java
+++ JavaFXJDBC/src/main/java/org/example/javafxjdbc/HelloController.java
@@ -1,14 +1,20 @@
 package org.example.javafxjdbc;
 
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
 import javafx.scene.control.Label;
 
+import java.sql.DatabaseMetaData;
+
 public class HelloController {
+
+    String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
+    private DatabaseMetaData DriverManager;
+    Connection conn = DriverManager.getConnection(url);
+    public BarChart barChart;
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
+
 }
