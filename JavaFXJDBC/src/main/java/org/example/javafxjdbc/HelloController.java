dd6be585c3211b5c32b92d657ae2a0df15c2f72f
Alex <agatter@htl-steyr.ac.at>
2026-01-21 12:14:11 +0100

    new

--- JavaFXJDBC/src/main/java/org/example/javafxjdbc/HelloController.java
+++ JavaFXJDBC/src/main/java/org/example/javafxjdbc/HelloController.java
@@ -2,19 +2,50 @@ package org.example.javafxjdbc;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
-import javafx.scene.control.Label;
+import javafx.scene.chart.XYChart;
 
-import java.sql.DatabaseMetaData;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class HelloController {
 
-    String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
-    private DatabaseMetaData DriverManager;
-    Connection conn = DriverManager.getConnection(url);
-    public BarChart barChart;
+
     @FXML
-    private Label welcomeText;
+    private BarChart<String, Number> barChart;
 
+    @FXML
+    public void initialize() {
 
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
 
+        String sql =
+                "SELECT (indepyear / 10) * 10 AS decade, COUNT(*) AS count " +
+                        "FROM country " +
+                        "WHERE indepyear IS NOT NULL AND indepyear > 0 " +
+                        "GROUP BY decade " +
+                        "ORDER BY decade";
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://xserv:5432/world2", "reader","reader");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String decade = rs.getInt("decade") + "s";
+                int count = rs.getInt("count");
+                series.getData().add(new XYChart.Data<>(decade, count));
+            }
+
+            barChart.getData().add(series);
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
